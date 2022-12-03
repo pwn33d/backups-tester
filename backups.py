7131daaf138ff16c2b45965751a3d3f2f1c2b246
pwn3d <31493122+pwn33d@users.noreply.github.com>
2022-12-03 09:25:07 -0800

    Add files via upload
    usage is python3 backupu.py https://sub.domain.com without / in the End! 
    
    i will edit this script as much as i can to make it faster

---
+++ backups.py
@@ -0,0 +1,35 @@
+import requests,sys,concurrent.futures,time
+def testspeed():
+    def statuscode(name, timeout=0.1):
+        try:
+            status = requests.get(url_name+"/"+name, allow_redirects=False)
+        except HTTPError as e:
+            return(e.code)
+        if "incident id" in status.text or "access denied" in status.text:
+                return False
+        elif len(status.text) <= 40:
+            return False
+        if status.status_code == 200:
+            print(status.url+"\n"+str(status.status_code))
+    try:
+        url_name = sys.argv[1]
+    except:
+        print("usage is python3 backups.py https://sub.domain.com without / in the end!")
+    else:
+        if url_name.startswith("https://"):
+            delete = url_name.replace("https://","")
+            regex = delete.split(".")[0]
+        elif url_name.startswith("http://"):
+            delete = url_name.replace("http://","")
+            regex = delete.split(".")[0]
+        end_points = ["gz","zip","tar","tar.gz","csv","doc","docx","xls","xlsx","sql","7z","rar","dump.sql","sql.tar.gz","sql.zip","bak","backup","bakup","old","tmp","log","db","sql.bak","zip.bak","zip.old"]
+        for i in end_points:
+            i = regex+"."+i
+            statuscode(i)
+        for i in end_points:
+            i = delete+"."+i
+            statuscode(i)
+
+#new line
+with concurrent.futures.ThreadPoolExecutor() as pl:
+    pl.submit(testspeed())
